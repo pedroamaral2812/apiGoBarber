997312e1ebd1d635b63396d284831d8d14d76dcc
Pedro Amaral <pedropaulo@life.com.br>
2019-08-24 07:35:37 -0300

    Migrations funcionando

---
+++ src/config/database.js
@@ -0,0 +1,13 @@
+module.exports = {
+  dialect: 'postgres',
+  host: 'localhost',
+  username:'postgres',
+  password: 'pedro1991',
+  database: 'gobarber',
+  define: {
+    timestamp: true,
+    underscored: true,
+    underscoredAll: true,
+  },
+
+};
