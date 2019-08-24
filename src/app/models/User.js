7242c2a6bcdf0c93f8d832a1f4358d23d3f28527
Pedro Amaral <pedropaulo@life.com.br>
2019-08-24 10:02:19 -0300

    bcypt funcionando

--- src/app/models/User.js
+++ src/app/models/User.js
@@ -1,15 +1,27 @@
 import Sequelize, { Model } from 'sequelize';
+import bcrypt from 'bcryptjs';
+import { async } from '../../../../../../../../AppData/Local/Microsoft/TypeScript/3.5/node_modules/rxjs/internal/scheduler/async';
 
 class User extends Model {
   static init(sequelize){
     super.init({
       name: Sequelize.STRING,
       email: Sequelize.STRING,
+      //Campo vitual são campos que nunca existirão de fato na tabela
+      password: Sequelize.VIRTUAL,
       password_hash: Sequelize.STRING,
       provider: Sequelize.BOOLEAN,
     },{
       sequelize,
     });
+
+    this.addHook('beforeSave', async user => {
+      if(user.password){
+        user.password_hash = await bcrypt.hash(user.password,8);
+      }
+    });
+
+    return this;
   }
 }
 
